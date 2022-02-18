c7d0dfa7675e016449a0725e6139e7d4bd03ea05
Daniel <danigonlinea@gmail.com>
2022-02-18 13:34:17 +0100

    add button with css custom properties

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <Button label="Click me" value="click me"> Click me </Button>
   </div>
 </template>
 
 <script>
+import { Button } from './components'
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Button,
+  },
 }
 </script>
 
--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -39,14 +39,25 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="css">
+:root {
+  --primary-color: #41b883;
+  --on-primary-color: white;
+
+  --small-spacing: 12px;
+  --normal-spacing: calc(var(--small-spacing) * 2);
+}
+
 button {
   border: 1px solid #eee;
   border-radius: 3px;
-  background-color: #ffffff;
+  background-color: var(--primary-color);
   cursor: pointer;
   font-size: 15pt;
   padding: 3px 10px;
   margin: 10px;
+  border: 4px solid var(--primary-color);
+  padding: var(--small-spacing) var(--normal-spacing);
+  transition: 0.25s ease-in-out all;
 }
 </style>

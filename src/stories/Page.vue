17438d9860c62cf8032226d7fe66f30fbe2ff63e
Daniel <danigonlinea@gmail.com>
2022-02-18 09:45:07 +0100

    passing eslint to files

--- src/stories/Page.vue
+++ src/stories/Page.vue
@@ -76,11 +76,11 @@
 </template>
 
 <script>
-import "./page.css";
-import MyHeader from "./Header.vue";
+import './page.css'
+import MyHeader from './Header.vue'
 
 export default {
-  name: "my-page",
+  name: 'my-page',
 
   components: { MyHeader },
 
@@ -92,14 +92,14 @@ export default {
 
   methods: {
     onLogin() {
-      this.$emit("onLogin");
+      this.$emit('onLogin')
     },
     onLogout() {
-      this.$emit("onLogout");
+      this.$emit('onLogout')
     },
     onCreateAccount() {
-      this.$emit("onCreateAccount");
+      this.$emit('onCreateAccount')
     },
   },
-};
+}
 </script>
